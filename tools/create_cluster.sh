da1876be10ddb6acf868cb98e9bb0d763f90aacd
MasayaAoyama <tennis.tenmasa@gmail.com>
2020-03-07 05:45:36 +0900

    add create cluster scripts

--- tools/create_cluster.sh
+++ tools/create_cluster.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-export CLUSTER=tb1
+if [ -z "$PROJECT" ]; then
+  echo "please set PROJECT env var";
+  exit 1;
+fi
+export CLUSTER=tb-$RANDOM
 export REGION=asia-northeast1
 
 gcloud beta container --project $PROJECT clusters create $CLUSTER --region $REGION --no-enable-basic-auth --release-channel "rapid" --machine-type "n1-standard-4" --image-type "UBUNTU" --disk-type "pd-ssd" --disk-size "150" --num-nodes "2" --enable-autoscaling --min-nodes "1" --max-nodes "10" --enable-autoupgrade --enable-autorepair
