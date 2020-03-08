dc859992e57919d9ca4650226f2b1c156cad6b6b
MasayaAoyama <tennis.tenmasa@gmail.com>
2020-03-08 19:22:01 +0900

    remove unused harbor2

--- tools/init_cluster.sh
+++ tools/init_cluster.sh
@@ -2,3 +2,4 @@
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 kubectl sort-manifests -R -f ${CURRENT_DIR}/../manifests/ | kubectl apply -f -
 
+kubectl sort-manifests -R -f ${CURRENT_DIR}/../manifests/ | kubectl apply -f -
