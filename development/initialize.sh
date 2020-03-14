3cf46e039cb01c062d46e80c8742e9658c5973a4
MasayaAoyama <tennis.tenmasa@gmail.com>
2020-03-15 01:40:22 +0900

    add grpc-web install script

--- development/initialize.sh
+++ development/initialize.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 OS_TYPE=$(uname | tr '[A-Z]' '[a-z]')
+GPRC_WEB_VERSION=1.0.7
+SKAFFOLD_VERSION=1.5.0
 
 # install developmen tools
 brew install protobuf
+
 go get -u github.com/golang/protobuf/protoc-gen-go
+
+curl -sL https://github.com/grpc/grpc-web/releases/download/${GPRC_WEB_VERSION}/protoc-gen-grpc-web-${GPRC_WEB_VERSION}-${OS_TYPE}-x86_64 -o /usr/local/bin/protoc-gen-grpc-web
+chmod 755 /usr/local/bin/protoc-gen-grpc-web
+
 
 # install skaffold
 if ! which skaffold; then
-  SKAFFOLD_VERSION=1.5.0
   curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/v${SKAFFOLD_VERSION}/skaffold-${OS_TYPE}-amd64
   chmod +x skaffold
   sudo mv skaffold /usr/local/bin
 fi
+
 
 # install telepresence
 if ! which telepresence; then
