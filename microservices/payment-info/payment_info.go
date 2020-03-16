c0d5afe631b54669bf642aa72685dbe5d3a1acc8
MasayaAoyama <tennis.tenmasa@gmail.com>
2020-03-17 07:08:10 +0900

    add Name field at PaymentInfo struct

--- microservices/payment-info/payment_info.go
+++ microservices/payment-info/payment_info.go
@@ -3,12 +3,13 @@ package main
 import "time"
 
 type PaymentInfo struct {
-	UUID        string `gorm:"primary_key"`
-	UserUUID    string
-	CardNumber  string
-	CommentUUID string
-	ProductUUID string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   *time.Time `sql:"index"`
+	UUID           string `gorm:"primary_key"`
+	UserUUID       string
+	Name           string
+	CardNumber     string
+	SecurityCode   string
+	ExpirationDate string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      *time.Time `sql:"index"`
 }
