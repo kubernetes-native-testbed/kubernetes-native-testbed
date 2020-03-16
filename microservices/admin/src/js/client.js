1e3b47c1bcf8e17f400927cb6492e9b9c3bc8d9d
MasayaAoyama <tennis.tenmasa@gmail.com>
2020-03-17 08:46:01 +0900

    add payment info admin pages

--- microservices/admin/src/js/client.js
+++ microservices/admin/src/js/client.js
@@ -6,4 +6,5 @@ const {Empty} = require('google-protobuf/google/protobuf/empty_pb.js');
 const product = require('./product.js');
 const user = require('./user.js');
 const order = require('./order.js');
+const payment_info = require('./payment_info.js');
 
