1949130e3e920927f60883110c6d920d95c43416
MasayaAoyama <tennis.tenmasa@gmail.com>
2020-03-20 13:07:13 +0900

    modify init scripts

--- tools/create_cluster.sh
+++ tools/create_cluster.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-if [ -z "$PROJECT" ]; then
-  echo "please set PROJECT env var";
+CURRENT_DIR=$(cd $(dirname $0); pwd)
+
+if [ -z "$KUBERNETES_PLATFORM" ]; then
+  echo "please set KUBERNETES_PLATFORM env var";
   exit 1;
 fi
-export CLUSTER=tb-$RANDOM
-export REGION=asia-northeast1
 
-gcloud beta container --project $PROJECT clusters create $CLUSTER --region $REGION --no-enable-basic-auth --release-channel "rapid" --machine-type "n1-standard-4" --image-type "UBUNTU" --disk-type "pd-ssd" --disk-size "150" --num-nodes "2" --enable-autoscaling --min-nodes "1" --max-nodes "10" --enable-autoupgrade --enable-autorepair --no-enable-stackdriver-kubernetes
-
-gcloud container clusters get-credentials $CLUSTER --region $REGION --project $PROJECT
-
-
+if [ $KUBERNETES_PLATFORM = "gke" ]; then
+  sh ${CURRENT_DIR}/create_cluster_gke.sh
+fi
--- tools/destroy_cluster.sh
+++ tools/destroy_cluster.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+if [ -z "$KUBERNETES_PLATFORM" ]; then
+  echo "please set KUBERNETES_PLATFORM env var";
+  exit 1;
+fi
+
+CURRENT_DIR=$(cd $(dirname $0); pwd)
+
 kubectl -n argocd delete applications --all
 kubectl -n projectcontour delete svc envoy
 
+if [ $KUBERNETES_PLATFORM = "gke" ]; then
+  sh ${CURRENT_DIR}/destroy_cluster_gke.sh
+fi
