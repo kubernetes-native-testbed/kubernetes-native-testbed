696f27bb20eefc61aa6eb3003545fcb264841010
Mizuki Urushida <z11111001011@gmail.com>
2020-03-21 19:16:30 +0900

    add commit section to admin

--- microservices/admin/src/js/cart.js
+++ microservices/admin/src/js/cart.js
@@ -1,4 +1,4 @@
-const {ShowRequest, ShowResponse, AddRequest, RemoveRequest, Cart} = require('./protobuf/cart_pb.js');
+const {ShowRequest, ShowResponse, AddRequest, RemoveRequest, CommitRequest, CommitResponse, Cart} = require('./protobuf/cart_pb.js');
 
 const {CartAPIClient} = require('./protobuf/cart_grpc_web_pb.js');
 
@@ -9,6 +9,11 @@ export const order = new Vue({
     form: {
       userUUID: '',
       cartProducts: [],
+    },
+    commitform: {
+      userUUID: '',
+      paymentInfoUUID: '',
+      addressUUID: '',
     },
     resp: {
       cart: [],
@@ -26,6 +31,11 @@ export const order = new Vue({
     clearForm: function() {
       this.form.userUUID = '';
       this.form.cartProducts = [];
+    },
+    clearCommitForm: function() {
+      this.commitform.userUUID = '';
+      this.commitform.paymentInfoUUID = '';
+      this.commitform.addressUUID = '';
     },
     clearResponseField: function() {
       this.resp.cart = [];
@@ -81,6 +91,48 @@ export const order = new Vue({
           this.resp.errorCode = err.code;
           this.resp.errorMsg = err.message;
         } else {
+          this.resp.errorCode = err.code;
+        }
+      });
+    },
+    getCartProducts: function(userUUID) {
+      return new Promise((resolve, reject) => {
+        const req = new ShowRequest();
+        req.setUseruuid(userUUID);
+        this.client.show(req, {}, (err, resp) => {
+          if (err) {
+            this.resp.errorCode = err.code;
+            this.resp.errorMsg = err.message;
+          } else {
+            resolve(resp.getCart().getCartproductsMap())
+          }
+        });
+      });
+    },
+    commitCart: async function() {
+      this.clearResponseField();
+      const req = new CommitRequest();
+      const c = new Cart();
+      c.setUseruuid(this.commitform.userUUID);
+      const cartProducts = await this.getCartProducts(this.commitform.userUUID);
+      console.log("cartProducts:", cartProducts);
+      cartProducts.forEach(function(count, productUUID) {
+        console.log("products uuid:", productUUID);
+        console.log("products count:", count);
+        c.getCartproductsMap().set(productUUID, count);
+      });
+      req.setCart(c);
+      req.setAddressuuid(this.commitform.addressUUID);
+      req.setPaymentinfouuid(this.commitform.paymentInfoUUID);
+      console.log("cart:", c);
+      this.client.commit(req, {}, (err, resp) => {
+        if (err) {
+          this.resp.errorCode = err.code;
+          this.resp.errorMsg = err.message;
+        } else {
+          let c = new Object();
+          c.orderUUID = resp.getOrderuuid();
+          this.resp.cart.push(c);
           this.resp.errorCode = err.code;
         }
       });
