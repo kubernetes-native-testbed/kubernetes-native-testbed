e3d561b270e76663d4e5460470a217c0f213ce89
MasayaAoyama <tennis.tenmasa@gmail.com>
2020-03-22 05:19:38 +0900

    add order kafka settings and so on

--- tools/create_cluster_gke.sh
+++ tools/create_cluster_gke.sh
@@ -6,7 +6,7 @@ fi
 export CLUSTER=${CLUSTER:-tb-$RANDOM}
 export GCP_REGION=${GCP_REGION:-asia-northeast1}
 
-gcloud beta container --project $GCP_PROJECT clusters create $CLUSTER --region $GCP_REGION --no-enable-basic-auth --release-channel "rapid" --machine-type "n1-standard-4" --image-type "UBUNTU" --disk-type "pd-ssd" --disk-size "150" --preemptible --num-nodes "2" --enable-autoscaling --min-nodes "1" --max-nodes "15" --enable-autoupgrade --enable-autorepair --no-enable-stackdriver-kubernetes --disable-addons HttpLoadBalancing
+gcloud beta container --project $GCP_PROJECT clusters create $CLUSTER --region $GCP_REGION --no-enable-basic-auth --release-channel "rapid" --machine-type "n1-standard-4" --image-type "UBUNTU" --disk-type "pd-ssd" --disk-size "50" --preemptible --num-nodes "3" --enable-autoscaling --min-nodes "1" --max-nodes "15" --enable-autoupgrade --enable-autorepair --no-enable-stackdriver-kubernetes --addons HorizontalPodAutoscaling
 
 gcloud container clusters get-credentials $CLUSTER --region $GCP_REGION --project $GCP_PROJECT
 
