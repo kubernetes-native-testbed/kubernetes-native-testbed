4beb9af0b263da6cedeff377ad0a691151b3a56a
MasayaAoyama <tennis.tenmasa@gmail.com>
2020-04-12 12:20:21 +0900

    increase security using gcp service account

--- tools/destroy_cluster.sh
+++ tools/destroy_cluster.sh
@@ -4,4 +4,9 @@ CURRENT_DIR=$(cd $(dirname $0); pwd)
 
 if [ $KUBERNETES_PLATFORM = "gke" ]; then
   gcloud container clusters delete ${CLUSTER_NAME} --region ${GCP_REGION} --quiet
+
+  export GCP_SA_NAME=testbed-gcp-sa
+  export GCP_SA_EMAIL=$(gcloud iam service-accounts list --filter="Name:${GCP_SA_NAME}" --format='value(email)')
+
+  gcloud iam service-accounts delete $GCP_SA_EMAIL --quiet
 fi
