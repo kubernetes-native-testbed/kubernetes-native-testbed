eeaba98af7bc5898f36f72c51cc1914c4d39f6dd
TomoyaKitaura <kitaura@supinf.co.jp>
2020-04-20 11:46:01 +0900

    fix fix Variable name

--- tools/init_repo.sh
+++ tools/init_repo.sh
@@ -10,11 +10,11 @@ git checkout develop
 for DIR in manifests microservices; do
   for FILE in `find ${CURRENT_DIR}/../${DIR}/ -type f`; do
     perl -pi -e "s|__LOADBALANCER_IP_ADDRESS__|${LOADBALANCER_IP_ADDRESS}|g" $FILE;
-    perl -pi -e "s|__TB_GITHUB_ORG_NAME__|${TB_GITHUB_ORG_NAME}|g" $FILE;
+    perl -pi -e "s|__TB_GITHUB_ORG_NAME__|${TB_GITHUB_USER}|g" $FILE;
   done
 done
 perl -pi -e "s|__LOADBALANCER_IP_ADDRESS__|${LOADBALANCER_IP_ADDRESS}|g" ${CURRENT_DIR}/../README.md
-perl -pi -e "s|__TB_GITHUB_ORG_NAME__|${TB_GITHUB_ORG_NAME}|g" ${CURRENT_DIR}/../README.md
+perl -pi -e "s|__TB_GITHUB_ORG_NAME__|${TB_GITHUB_USER}|g" ${CURRENT_DIR}/../README.md
 
 git add --all
 git commit -m "initialized repo"
