c4e60727137545305692a66ce2bd540afe9bcda1
Naoki Oketani <okepy.naoki@gmail.com>
2020-04-29 09:52:26 +0900

    run `gcloud services enable container.googleapis.com` in tools/init.sh

--- tools/init.sh
+++ tools/init.sh
@@ -6,6 +6,6 @@ CURRENT_DIR=$(cd $(dirname $0); pwd)
 source ${CURRENT_DIR}/env
 
 ${CURRENT_DIR}/init_cli.sh
+${CURRENT_DIR}/enable_service.sh
 ${CURRENT_DIR}/allocate_staticip.sh
 ${CURRENT_DIR}/init_repo.sh
-
